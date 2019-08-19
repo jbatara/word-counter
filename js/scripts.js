841df835804b38ba492c9488128c8fcae72c0992
Jennifer Batara and Jacqueline Remmel <jacqueline@>
2019-08-19 14:53:05 -0700

    Adds output to html.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,27 @@ $(document).ready(function() {
   $("#input").submit(function(event) {
     event.preventDefault();
     var userParagraph = $("#paragraph").val();
-
-    
-
-
-    $("#output").text(userParagraph);
+    var paragraphArray = userParagraph.split(" ");
+    var noPunctuation = paragraphArray.map(function(word) {
+      return word.replace(/[^a-zA-Z']/g, "");
+    });
+    var noEmptyWords = [];
+    noPunctuation.forEach(function(word) {
+      if (word) {
+        noEmptyWords.push(word);
+      }
+    });
+    var wordTally = {};
+    noEmptyWords.forEach(function(word) {
+      if (Object.keys(wordTally).includes(word)) {
+        wordTally[word] += 1;
+      } else {
+        wordTally[word] = 1;
+      }
+    });
+    Object.keys(wordTally).forEach(function(key) {
+      $("#output").append("<p>" + key + ": " + wordTally[key] +"</p>");
+    });
     $("#output").show();
 
   });
