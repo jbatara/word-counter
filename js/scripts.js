588e4d5cbeb446bfea93358afa6a989279bb3894
Jennifer Batara and Jacqueline Remmel <jennifer@>
2019-08-19 13:30:10 -0700

    Update CSS styling and output div in html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,17 @@
 $(document).ready(function() {
   $("#input").submit(function(event) {
     event.preventDefault();
+    var userParagraph = $("#paragraph").val();
+
+    
+
+
+    $("#output").text(userParagraph);
+    $("#output").show();
 
   });
   $("#clear").click(function() {
     $("#input").trigger("reset");
-    
+    $("#output").hide();
   });
 });
