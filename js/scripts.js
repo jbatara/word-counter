4037f60381c0ccedac025334f1fe0ee2c9be74f0
Jennifer Batara and Jacqueline Remmel <jacqueline@>
2019-08-19 13:18:20 -0700

    Add clear button functionality in JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
   $("#input").submit(function(event) {
     event.preventDefault();
+
+  });
+  $("#clear").click(function() {
+    $("#input").trigger("reset");
     
   });
 });
